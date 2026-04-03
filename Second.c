51ba1e7051547d17c9741809b9f25a858c4045cd
muntaha994 <sidratulmuntahanidhi011@gmail.com>
2026-04-03 20:55:24 +0600

    added Second.c

---
+++ Second.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++){
+    scanf("%d",&arr[i]);
+}
+int count=0;
+for(int i=0;i<n;i++){
+
+if(arr[i]==0){
+ printf("%d\n",i);
+}
+else{
+
+    printf("SAD\n");
+}
+}
+
+
+return 0;
+}
